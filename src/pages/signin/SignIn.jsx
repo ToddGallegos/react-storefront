02d1dd7afd1ca42d80ddfb739c3bf9873fba5a60
ToddGallegos <toroga@hotmail.com>
2023-02-13 13:16:18 -0800

    finally able to create new user from react

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from 'react';
+import "./signin.css"
 
 export default function SignIn() {
+  const signIn = async (event) => {
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+
+    const reqBody = {
+      username: username,
+      password: password
+    }
+
+    const url = 'http://localhost:5000/meanmugsapi/signin'
+    const options = {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": 'application/json'
+      }
+    }
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    console.log(data)
+  }
+
+
+
   return (
-    <div>SignIn</div>
+    <div className="signInContainer">
+      <h1>Sign In</h1>
+      <form method="post">
+        <label htmlFor="username">Username: </label>
+        <input autoFocus="True" type="text" name="username" id="username" />
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" id="password" />
+        <button onClick={signIn} id="signupSubmit" type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from 'react';
+import "./signup.css"
 
 export default function SignUp() {
+  const signUp = async (event) => {
+    event.preventDefault();
+    console.log(event.target)
+    console.log(event.target.elements["username"].value)
+    const username = event.target.elements["username"].value;
+    const email = event.target.elements["email"].value;
+    const password = event.target.elements["password"].value;
+    const confirmPassword = event.target.elements["passwordCheck"].value;
+
+    const reqBody = {
+      username: username,
+      email: email,
+      password: password
+    }
+
+    if (password !== confirmPassword) {
+      console.log("passwords don't match")
+    }
+
+    const response = await fetch('http://localhost:5000/meanmugsapi/signup', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(reqBody)
+    });
+    const data = await response.json();
+    console.log(data)
+  }
+
+
+
   return (
-    <div>SignUp</div>
+    <div className="signUpContainer">
+      <h1>Sign Up</h1>
+      <form onSubmit={signUp}>
+        <label htmlFor="username">Username: </label>
+        <input autoFocus="True" type="text" name="username" id="username" />
+        <label htmlFor="email">Email: </label>
+        <input type="text" name="email" id="email" />
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" id="password" />
+        <label htmlFor="passwordCheck">Confirm Password: </label>
+        <input type="password" name="passwordCheck" id="passwordCheck" />
+        <button id="signupSubmit" type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
